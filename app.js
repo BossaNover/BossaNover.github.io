f0b68e9e38e17a0e394374488198e4b823b9b548
Vitaly Izvekov <bossanover@yandex.ru>
2019-12-26 22:51:44 +0300

    last mistakes in phase-2

---
+++ app.js
@@ -0,0 +1,9 @@
+var hamburger = document.querySelector('.hamburger-menu');
+
+console.log(hamburger.elements);
+
+// hamburger.addEventListener('click', ()=> {
+  
+//   var header = document.querySelector('.header');
+//     console.log(header);
+//   })
