c7e0a752294cfdea6c2689209ba16a5c98498660
Vitaly Izvekov <bossanover@yandex.ru>
2019-12-27 13:20:24 +0300

    js started

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var hamburger = document.querySelector('.hamburger-menu');
+var hamburger = document.querySelector('.hamburger-menu-link');
 
-console.log(hamburger.elements);
+console.log(hamburger);
 
-// hamburger.addEventListener('click', ()=> {
-  
-//   var header = document.querySelector('.header');
-//     console.log(header);
-//   })
+  hamburger.addEventListener('click', (event)=> {
+    console.log(event.type);
+    console.log(event.preventDefault);
+    
+   var header = document.querySelector('.header');
+    console.log(header);
+  })
