4362518cd6fb6c9fb55973fa2fc759488aafae24
Vitaly Izvekov <bossanover@yandex.ru>
2019-12-29 17:13:16 +0300

    new section burger draft

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var hamburger = document.querySelector('.hamburger-menu-link');
+let hamburger = document.querySelector('.hamburger-menu-link');
+let close = document.querySelector('.close');
+let header = document.querySelector('.header'); // здесь ставим точку, т.к.м.б. ещё и id, кроме класса.
 
-console.log(hamburger);
+const clicker = () => {
+  header.classList.toggle('header_fullscreen'); //здесь точки не надо ставить, т.к. метод для класса -classList
+};  //toggle - это переключатель. Он заменяет методы add и remove.
 
-  hamburger.addEventListener('click', (event)=> {
-    console.log(event.type);
-    console.log(event.preventDefault);
-    
-   var header = document.querySelector('.header');
-    console.log(header);
-  })
+hamburger.addEventListener('click', clicker); 
+close.addEventListener('click', clicker);
