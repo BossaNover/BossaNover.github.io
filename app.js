528e8f503101bf52feb3a13ea3709cf9800c4503
Vitaly Izvekov <bossanover@yandex.ru>
2019-12-31 02:22:13 +0300

    section burgers added

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ const clicker = () => {
 
 hamburger.addEventListener('click', clicker); 
 close.addEventListener('click', clicker);
+
+
+let burgersCompHover = document.querySelector('.burgers-comp__hover');
+console.log(burgersCompHover.elements);
+
+let burgersComp = document.querySelector('.burgers-comp');
+
+console.log(burgersComp.elements);
+
+//let burgersCompClose = document.querySelector('.burgers-comp__close');
+//let burgersCompHover = document.querySelector('.burgers-comp__hover');
+
+//burgersCompClose.style.offsetHeight = "0px";
+
+
+//burgersCompClose.addEventListener('click', function (e) {   
+//  burgersCompHover.offsetHeight = '0px';       // --так не работает, почему -непонятно. style.offsetHeight тоже.
+//} )
+
+//console.log(burgersCompHover.offsetHeight); - а это свойство работает (выше почему-то нет, только на экран выводит)
